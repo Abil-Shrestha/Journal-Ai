f2ece581a15f355979b2cce92fb4cb02a13a5f53
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-10-11 20:14:08 -0400

    new user routing

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-**JournalAi*
+**JournalAi**
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user' 
+
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-black text-white">
       <div className='w-full max-w-[640px] mx-auto'>
         <h1 className='text-6xl mb-4'>The best Journal App</h1>
         <p className='text-2xl text-white/60 mb-4'>This is the best app for tracking your mood througout you life. All you have to do is be honest</p>
         <div>
-          <Link href='/journal'>
+          <Link href={href}>
             <button className='bg-purple-700 rounded-xl px-4 py-2 text-lg'>Get started</button>
           </Link>
         </div>
