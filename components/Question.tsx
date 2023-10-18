1ae75a2053370bcc0d087c3e23c335b8f457c089
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-10-18 00:30:05 -0400

    Vector similarity search langchain

--- components/Question.tsx
+++ components/Question.tsx
@@ -4,21 +4,25 @@ import React, { useState } from 'react'
 const Question = () => {
     const [value,setValue] = useState('')
     const onChange= (e:any) => {
+        setValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
         e.preventDefault()
-
-
+        
     }
   return (
     <div>
-        <form>
-            <input 
+        <form onSubmit={handleSubmit}>
+            <input
+            onChange={onChange}
             type="text"
             placeholder="ask a question"
             value={value}
             className='border border-black/20 px-4 py-2 text-lg rounded-lg '/>
             <button 
             type="submit" 
-            className='bg-blue-400/20 px-4 py-2 rounded-lg text-lg ml-2'>Ask</button>
+            className='bg-neutral-400/20 px-4 py-2 rounded-lg text-lg ml-2'>Ask</button>
         </form>
     </div>
   )
--- utils/ai.ts
+++ utils/ai.ts
@@ -2,6 +2,11 @@ import { OpenAI } from 'langchain/llms/openai'
 import { z } from 'zod'
 import { StructuredOutputParser } from 'langchain/output_parsers'
 import { PromptTemplate } from 'langchain/prompts'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+
 
 const parser = StructuredOutputParser.fromZodSchema(
     z.object({
@@ -41,4 +46,24 @@ export const analyse = async(prompt:any) => {
     } catch (e) {
         console.error(e)
     }
+}
+
+const qa = async (question, entries) => {
+    const docs = entries.map (entry => {
+        return new Document({
+            pageContent: entry.content,
+            metadata: {id: entry.id, createdAt: entry.createdAt}
+        })
+    })
+    const model = new OpenAI({temperature: 0, modelName:'gpt-3.5-turbo'})
+    const chain = loadQARefineChain(model)
+    const embeddings = new OpenAIEmbeddings()
+    const store = await MemoryVectorStore.fromDocuments(docs,embeddings)
+    const relevantDocs = await store.similaritySearch(question)
+    const res = await chain.call({
+        input_documnets:relevantDocs,
+        question,
+    })
+
+    return res.output_text
 }
